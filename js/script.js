bbc0d33caa8887b8f37ff1f1fc0d916a87b79c4c
Przemek Szadkowski <przemoszadkowski@o2.pl>
2018-12-11 10:31:00 +0100

    Add JS to photos - changes

--- js/script.js
+++ js/script.js
@@ -10,96 +10,103 @@ const startButton = document.getElementById("start");
 
 // events - buttons
 
-bars.addEventListener("click", function () {
-    bars.classList.add("off");
-    close.classList.remove("off");
-    menu.classList.remove("off");
-})
-close.addEventListener("click", function () {
+bars.addEventListener("click", function() {
+  bars.classList.add("off");
+  close.classList.remove("off");
+  menu.classList.remove("off");
+});
+close.addEventListener("click", function() {
+  close.classList.add("off");
+  bars.classList.remove("off");
+  menu.classList.add("off");
+});
+
+menuButtons.forEach(function(menuButton) {
+  menuButton.addEventListener("click", function() {
     close.classList.add("off");
     bars.classList.remove("off");
     menu.classList.add("off");
-})
-
-menuButtons.forEach(function (menuButton) {
-    menuButton.addEventListener("click", function () {
-        close.classList.add("off");
-        bars.classList.remove("off");
-        menu.classList.add("off");
-    })
-})
+  });
+});
 
 // modal
 
-regButton.addEventListener("click", function () {
-    modal.classList.remove("off");
-    closeButton.classList.add("on");
-    modalWrapper.classList.add("active");
-    close.classList.add("off");
-    bars.classList.remove("off");
-    menu.classList.add("off");
-})
+regButton.addEventListener("click", function() {
+  modal.classList.remove("off");
+  closeButton.classList.add("on");
+  modalWrapper.classList.add("active");
+  close.classList.add("off");
+  bars.classList.remove("off");
+  menu.classList.add("off");
+});
 
-closeButton.addEventListener("click", function () {
-    modal.classList.add("off");
-    closeButton.classList.remove("on");
-    modalWrapper.classList.remove("active");
-})
+closeButton.addEventListener("click", function() {
+  modal.classList.add("off");
+  closeButton.classList.remove("on");
+  modalWrapper.classList.remove("active");
+});
 
 // start button
 
-startButton.addEventListener("click", function () {
-    close.classList.add("off");
-    bars.classList.remove("off");
-    menu.classList.add("off");
-})
+startButton.addEventListener("click", function() {
+  close.classList.add("off");
+  bars.classList.remove("off");
+  menu.classList.add("off");
+});
 
 // scrolling
 
-$("#start").on("click", function () {
-    $("body, html").animate({
-        scrollTop: $("#top").offset().top,
-    }, 1000)
-})
+$("#start").on("click", function() {
+  $("body, html").animate(
+    {
+      scrollTop: $("#top").offset().top
+    },
+    1000
+  );
+});
 
-$(".fa-angle-double-up").on("click", function () {
-    $("body, html").animate({
-        scrollTop: $("#main_title").offset().top,
-    }, 1000)
-})
+$(".fa-angle-double-up").on("click", function() {
+  $("body, html").animate(
+    {
+      scrollTop: $("#main_title").offset().top
+    },
+    1000
+  );
+});
 
 // functions
 
 function showPhotos() {
-    const scrollPosition = window.scrollY;
-    const photoHeight = document.querySelector(".photo").clientHeight;
-    const photoPosition = document.querySelector(".photo").offsetTop;
-    const items = document.querySelectorAll(".photo");
+  //   debugger;
+  const scrollPosition = window.scrollY;
+  const photoHeight = document.querySelector(".photo").clientHeight;
+  const photoPosition = document.querySelector(".photo").offsetTop;
+  const items = document.querySelectorAll(".photo");
 
-    items.forEach(function (item) {
-        if (scrollPosition + 100 > photoPosition - photoHeight) {
-            item.classList.remove("off");
-        }
-        if ((scrollPosition + 200) < photoPosition - photoHeight) {
-            item.classList.add("off");
-        }
-    })
+  items.forEach(function(item) {
+    if (scrollPosition + 100 > photoPosition - photoHeight) {
+      item.classList.remove("off");
+    }
+    if (scrollPosition + 200 < photoPosition - photoHeight) {
+      item.classList.add("off");
+    }
+  });
 }
 
 function movePhotos() {
-    const scrollPosition = window.scrollY;
-    const partsHeight = document.querySelector(".part").clientHeight;
-    const partsPosition = document.querySelector(".part").offsetTop;
-    const items = document.querySelectorAll(".part");
+  const scrollPosition = window.scrollY;
+  const partsHeight = document.querySelector(".part").clientHeight;
+  const partsPosition = document.querySelector(".part").offsetTop;
+  const items = document.querySelectorAll(".part");
 
-    items.forEach(function (item) {
-        if (scrollPosition + 200 > partsPosition - partsHeight) {
-            item.classList.add("active");
-        }
-        if (scrollPosition + 600 < partsPosition - partsHeight) {
-            item.classList.remove("active");
-        }
-    })
+  items.forEach(function(item) {
+    if (scrollPosition + 200 > partsPosition - partsHeight) {
+      item.classList.add("active");
+    }
+    if (scrollPosition + 600 < partsPosition - partsHeight) {
+      item.classList.remove("active");
+    }
+  });
 }
 
 window.addEventListener("scroll", showPhotos);
